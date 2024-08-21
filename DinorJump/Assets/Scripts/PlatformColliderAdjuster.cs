d57334e1b83a38397a1153e25c17d714a27d3b67
Đoàn Thành Cao Nguyên <2151120042@ut.edu.vn>
2024-08-21 17:25:42 +0700

    Adding level 2

---
+++ DinorJump/Assets/Scripts/PlatformColliderAdjuster.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SVS.Platform
+{
+    public class PlatformColliderAdjuster : MonoBehaviour
+    {
+        [SerializeField]
+        private BoxCollider2D platformCollider;
+        [SerializeField]
+        private SpriteRenderer spriteRenderer;
+        private void Awake()
+        {
+            if (!platformCollider)
+            {
+                platformCollider = GetComponent<BoxCollider2D>();
+            }
+            if (!spriteRenderer)
+            {
+                spriteRenderer = GetComponent<SpriteRenderer>();
+            }
+        }
+        private void Start()
+        {
+            platformCollider.size = new Vector2(spriteRenderer.size.x,platformCollider.size.y);
+            platformCollider.offset = new Vector2(0, spriteRenderer.size.y / 2f - platformCollider.size.y / 2f);
+        }
+        //private void OnDrawGizmos()
+        //{
+        //    if (spriteRenderer == null)
+        //        return;
+
+        //    Gizmos.color = Color.red;
+        //    Gizmos.DrawCube(spriteRenderer.bounds.center, spriteRenderer.bounds.size);
+        //    Gizmos.color = Color.blue;
+        //    Gizmos.DrawSphere(spriteRenderer.bounds.center,1);
+        //}
+    }
+}
